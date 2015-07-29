353831e4b6339ba0a3d2129e497ad64480e94e8b
Emily Carson <emilyjanecarson@gmail.com>
2015-07-29 12:59:25 +0100

    Started player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,4 +1,20 @@
+require_relative 'board'
+
 class Player
+
+	attr_reader :hits
+
+	def initialize
+		@hits = []
+	end
+
 	def fire(coordinates)
+		@ships.values.each do |ship|
+			if (ship & coordinates.to_a) == []
+				@misses << coordinates
+			else
+				@hits << coordinates
+			end
+		end
 	end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,7 +1,19 @@
 require 'player'
+require 'ship'
 
 describe Player do
 	it "should respond to fire" do
 		expect(subject).to respond_to(:fire).with(1).argument
+	end
+	it "should fire to a co-ordinate on the board" do
+	end
+	it "hits a ship if it's on the board" do
+		ship = Ship.new
+		board = Board.new
+		board.place(ship,"A2", :E)
+		subject.fire("A3")
+		expect(subject.hits).to include "A3"
+	end
+	it "misses a ship if it's not on the board" do
 	end
 end
